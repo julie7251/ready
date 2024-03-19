36e255d694d821da792cb588651604cf593bda28
jiwon <wldnjs5391@gmail.com>
2024-03-19 15:43:27 +0900

    섹션4 삭제 / 섹션3 pc 작업 완료 / 반응형 작업중

--- js/main.js
+++ js/main.js
@@ -30,32 +30,37 @@ $(function () {
     afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
       console.log(`afterSlideLoad!`);
     },
-    onSlideLeave: function (
-      anchorLink,
-      index,
-      slideIndex,
-      direction,
-      nextSlideIndex
-    ) {
+    onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
       console.log(`onSlideLeave!`);
     },
   });
-  $(".singup-i").click(function () {
-    $(".signup li").fadeToggle(600);
-  });
-  $(".language-i").click(function () {
-    $(".language li").fadeToggle(600);
+});
+
+$(".ani-03").each(function () {
+  let text = this;
+  text.innerHTML = text.textContent.replace(/\S/g, "<i>$&</i>");
+  $(this)
+    .find("i")
+    .each(function (index, item) {
+      $(this).addClass("num" + index);
+      let i = index / 14;
+      $(this).css("animation-delay", i + 0.8 + "s");
+    });
+});
+// 섹션-3 마우스 호버 이미지 교체
+document.addEventListener("DOMContentLoaded", function () {
+  const targetImgs = document.querySelectorAll(".target-img");
+  const targetHovers = document.querySelectorAll(".target-hover");
+
+  targetImgs.forEach((img, index) => {
+    img.addEventListener("mouseover", function () {
+      img.style.display = "none";
+      targetHovers[index].style.display = "block";
+    });
+
+    targetHovers[index].addEventListener("mouseout", function () {
+      img.style.display = "inline-block"; // or whatever the original display property was
+      targetHovers[index].style.display = "none";
+    });
   });
 });
-// window.onload =function(){
-//   document.querySelector('.singup-i').addEventListener('click', function() {
-//     var signupItems = document.querySelectorAll('.signup li');
-//     signupItems.forEach(function(item) {
-//       if (item.style.display === 'none') {
-//         item.style.display = 'block';
-//       } else {
-//         item.style.display = 'none';
-//       }
-//     });
-//   });
-// }
